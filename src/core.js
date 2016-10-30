d1a549ee6213313421280ce07742e6b00ec578db
Leo Huang <lhuang@atlassian.com>
2016-10-30 18:38:49 +1100

    Add server side word generation

--- src/core.js
+++ src/core.js
@@ -1,5 +1,7 @@
 import {Map, List, fromJS} from 'immutable';
 
+let wordList = require('./words.json');
+
 function otherTeam(team) {
   return team === Game.TEAM_RED ? Game.TEAM_BLUE : Game.TEAM_RED;
 }
@@ -150,7 +152,66 @@ export {
   Game
 }
 
+/**
+ * From http://stackoverflow.com/questions/6274339/how-can-i-shuffle-an-array-in-javascript
+ */
+function shuffle(array) {
+  let counter = array.length;
+
+  // While there are elements in the array
+  while (counter > 0) {
+    // Pick a random index
+    let index = Math.floor(Math.random() * counter);
+
+    // Decrease counter by 1
+    counter--;
+
+    // And swap the last element with it
+    let temp = array[counter];
+    array[counter] = array[index];
+    array[index] = temp;
+  }
+
+  return array;
+}
+
+function generateRandomCards() {
+  shuffle(wordList);
+
+  let words = [];
+
+  let teams = [{
+    name: Game.TEAM_RED,
+    number: 8
+  }, {
+    name: Game.TEAM_BLUE,
+    number: 7
+  }, {
+    name: Game.ASSASSIN,
+    number: 1
+  }, {
+    name: Game.CIVILIAN,
+    number: 9
+  }];
+
+  let ptr = 0;
+  teams.forEach(team => {
+    for (let i = 0; i < team.number; i++) {
+      words.push({
+        word: wordList[ptr++],
+        team: team.name,
+        revealed: false
+      });
+    }
+  });
+
+  shuffle(words);
+
+  return fromJS(words);
+}
+
 export const INITIAL_STATE = Map({
+  cards: generateRandomCards(),
   players: Map({
     red: Map({
       agents: List()
